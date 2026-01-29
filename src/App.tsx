32a0f3de6115778c0e144c19a3fe6a8f532de32f
WillaG-E <willa.galipeaueldridge@maine.edu>
2026-01-29 09:52:26 -0500

    Added the Basic HTML and CSS tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,12 +8,53 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>This is heading text.</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>Hello World</p>
+            <img
+                src="../public/Poppies.jpg"
+                alt="A picture of a field of red poppies"
+            />
             <p>Willa Galipeau-Eldridge</p>
+            <ul>
+                <li>Lupine</li>
+                <li>Aster</li>
+                <li>Daisy</li>
+            </ul>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
