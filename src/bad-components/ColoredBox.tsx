db4ff8a005a159d9cf0e2c0bed8cd3e8c99bd5f4
WillaG-E <willa.galipeaueldridge@maine.edu>
2026-03-23 20:25:39 -0400

    Fixed merge conflict resolution errors

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -5,12 +5,6 @@ export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
 function ChangeColor({
-    nextColor,
-}: {
-    nextColor: () => void;
-}): React.JSX.Element {
-    return <Button onClick={nextColor}>Next Color</Button>;
-function ChangeColor({
     setIndex,
     index,
 }: {
@@ -28,7 +22,6 @@ function ChangeColor({
     );
 }
 
-function ColorPreview({ color }: { color: string }): React.JSX.Element {
 function ColorPreview({ index }: { index: number }): React.JSX.Element {
     return (
         <div
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,42 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function Doubler({
-    dhValue,
-    setDhValue,
-}: {
-    dhValue: number;
-    setDhValue: (value: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver({
-    dhValue,
-    setDhValue,
-}: {
-    dhValue: number;
-    setDhValue: (value: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
-
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
 
@@ -47,15 +11,14 @@ export function DoubleHalf(): React.JSX.Element {
         setDhValue(0.5 * dhValue);
     };
 
-    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
-            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
+            <Button onClick={double}>Double</Button>
+            <Button onClick={halve}>Halve</Button>
         </div>
     );
 }
